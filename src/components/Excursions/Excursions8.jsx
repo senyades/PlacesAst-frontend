03103d78ccd5b0c8094eb15480b3c68d30e9d111
Arseny Sergeev <80630560+senyades@users.noreply.github.com>
2025-06-15 22:47:52 +0300

    Update Excursions8.jsx

--- src/components/Excursions/Excursions8.jsx
+++ src/components/Excursions/Excursions8.jsx
@@ -72,13 +72,7 @@ const Excursions8 = () => {
       </header>
 
       <div className="viewer-container">
-        <iframe 
-          width="90%" 
-          height="500px" 
-          allowFullScreen="true" 
-          allow="accelerometer; magnetometer; gyroscope" 
-          src="https://panoraven.com/en/embed/rGGR6sHPoS"
-        ></iframe>
+        <iframe width="100%" height="640" frameborder="0" allow="xr-spatial-tracking; gyroscope; accelerometer" allowfullscreen scrolling="no" src="https://kuula.co/share/hHHnn?logo=1&info=1&fs=1&vr=0&sd=1&thumbs=1"></iframe>
       </div>
 
       <div className="audio-player">
